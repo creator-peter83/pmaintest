a3381b601a2d46dc487ea1c4b832f0fa2c8c4cbe
creator-peter83 <sungjun38@gmail.com>
2026-05-15 12:29:24 +0900

    Delete assets/js directory

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  const cards = document.querySelectorAll('.category-card, .product-card');
-  cards.forEach((card) => {
-    card.addEventListener('focusin', () => card.classList.add('is-focused'));
-    card.addEventListener('focusout', () => card.classList.remove('is-focused'));
-  });
-})();
